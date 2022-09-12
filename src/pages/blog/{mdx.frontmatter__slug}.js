64b5c45732a9bc45852c23c762d0c63dbec0c953
Minjae Joe(Cho) <ussr1285@ajou.ac.kr>
2022-09-12 16:04:18 +0900

    video on markdown test

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -1,13 +1,24 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
+import Video from "../components/video"
+
 
 const BlogPost = ({ data, children }) => {
-
+  const image = getImage(data.mdx.frontmatter.hero_image)
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      {/*<GatsbyImage
+        image={image}
+        alt={data.mdx.frontmatter.hero_image_alt}
+      />*/}
+      <Video
+        videoSrcURL={data.mdx.frontmatter.videoSrcURL}
+        videoTitle={data.mdx.frontmatter.videoTitle}
+      />
       {children}
     </Layout>
   )
@@ -19,12 +30,20 @@ query($id: String) {
       frontmatter {
         title
         date(formatString: "MMMM DD, YYYY")
+        videoSrcURL
+        videoTitle
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
     }
   }
 `
-
-
 
 export const Head = ({ data }) => <Seo title={data.mdx.frontmatter.title} />
 
