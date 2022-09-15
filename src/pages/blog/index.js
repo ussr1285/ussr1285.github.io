0bf08f48cdfc879d4d31ffe68c127c8ea3559987
Minjae Joe(Cho) <ussr1285@ajou.ac.kr>
2022-09-15 20:28:58 +0900

    링크 밑줄 없애기.

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { Link, graphql } from 'gatsby'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
+import navLinkItem from './index.module.css'
 
 const BlogPage = ({ data }) => {
     return (
@@ -10,7 +11,7 @@ const BlogPage = ({ data }) => {
                 {
                     data.allMdx.nodes.map((node) => (
                         <article key={node.id}>
-                            <h2>
+                            <h2 className={navLinkItem}>
                               <Link to={`/blog/${node.frontmatter.slug}`}>
                                 {node.frontmatter.title}
                               </Link>
