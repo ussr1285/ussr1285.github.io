04bf2af428cf4652bc6bcbfce1d421099e27d248
Minjae Joe(Cho) <ussr1285@ajou.ac.kr>
2022-09-16 23:38:08 +0900

    graphql blog 게시물들 정렬 조건 추가

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -30,7 +30,7 @@ const BlogPage = ({ data }) => {
 
 export const query = graphql`
   query {
-    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+    allMdx(sort: {fields: [frontmatter___date, frontmatter___slug], order: [DESC, DESC]}) {
       nodes {
         frontmatter {
           date(formatString: "MMMM D, YYYY")
