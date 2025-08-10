d0ee2c1f58caeeac74732816dcbe511791fcbd10
Minjae Joe <ipszone0525@gmail.com>
2025-08-10 23:17:42 +0900

    ADD: はちゅねミク tenor gif

--- pages/index.js
+++ pages/index.js
@@ -22,9 +22,13 @@ export default function Home() {
       <Head>
         <title>환영합니다</title>
         <meta name="description" content="개인 블로그" />
+        <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
       </Head>
 
       <div className="landing">
+        <div className="tenor-gif-embed" data-postid="27091473" data-share-method="host" data-aspect-ratio="0.75" data-width="200px" style={{marginBottom: '20px'}}>
+          <a href="https://tenor.com/view/%E5%88%9D%E9%9F%B3%E3%83%9F%E3%82%AF-gif-27091473">初音ミク GIF</a> from <a href="https://tenor.com/search/%E5%88%9D%E9%9F%B3%E3%83%9F%E3%82%AF-gifs">初音ミク GIFs</a>
+        </div>
         <h1>환영합니다</h1>
 
         <nav className="nav">
