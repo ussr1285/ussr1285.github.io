7ee45c04ac4fe00e1985dd45d42d7526994a928c
Minjae Joe <ipszone0525@gmail.com>
2025-08-10 22:50:36 +0900

    .

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,21 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import Link from 'next/link'
-import Head from 'next/head'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Head from "next/head";
 
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
     const handleKeyPress = (event) => {
-      if (event.key === 'Enter') {
-        router.push('/home')
+      if (event.key === "Enter") {
+        router.push("/home");
       }
-    }
-    
-    window.addEventListener('keydown', handleKeyPress)
-    return () => window.removeEventListener('keydown', handleKeyPress)
-  }, [router])
+    };
+
+    window.addEventListener("keydown", handleKeyPress);
+    return () => window.removeEventListener("keydown", handleKeyPress);
+  }, [router]);
 
   return (
     <>
@@ -23,20 +23,18 @@ export default function Home() {
         <title>환영합니다</title>
         <meta name="description" content="개인 블로그" />
       </Head>
-      
+
       <div className="landing">
         <h1>환영합니다</h1>
-        
+
         <nav className="nav">
           <Link href="/home">home</Link>
           <Link href="/about">about</Link>
           <Link href="/mogakso">mogakso</Link>
         </nav>
-        
-        <div className="enter-hint">
-          Press Enter to continue
-        </div>
+
+        <div className="enter-hint">Press Enter to continue</div>
       </div>
     </>
-  )
+  );
 }
